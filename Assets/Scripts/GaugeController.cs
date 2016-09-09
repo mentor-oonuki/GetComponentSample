b2affc85d527e0ddcdd76809b41e8b8e4c59b9bc
mentor-oonuki <mentor.oonuki@gmail.com>
2016-09-09 22:37:09 +0900

    【初期登録】

---
+++ Assets/Scripts/GaugeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GaugeController : MonoBehaviour {
+
+    private Slider slider;  // ゲージを表現するためのSliderオブジェクト
+
+    void Start()
+    {
+        // ゲージを表現するためのSliderオブジェクト
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    // 【重要】ScoreCounterからGetComponentで呼び出される関数
+    public void SetValue(int score)
+    {
+        // ゲージの長さをScoreにて設定
+        slider.value = score;
+    }
+}
---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreCounter : MonoBehaviour {
+
+    private int Score;              // 現在のスコア
+    private Text ScoreText;         // スコアを表示するためのTextコンポーネント
+    private Slider GaugeObject;     // ゲージを表示するためのオブジェクト
+
+    void Start()
+    {
+        // スコアを0で初期化する
+        Score = 0;
+        // スコアを表示するためのTextコンポーネントを取得する
+        ScoreText = gameObject.GetComponent<Text>();
+        // ゲージを表示するためのオブジェクトを取得する
+        GaugeObject = GameObject.FindObjectOfType<Slider>();
+    }
+
+    void Update()
+    {
+        // スコアを1加算する
+        Score++;
+        // スコアが1000を超えたら0に戻す
+        if(Score > 1000)
+        {
+            Score = 0;
+        }
+        // スコアをTextコンポーネントに設定する(画面に表示する)
+        ScoreText.text = Score.ToString();
+
+        // 【重要】GaugeControllerスクリプトSetValue()関数を呼び出す
+        GaugeObject.GetComponent<GaugeController>().SetValue(Score);
+    }
+
+}
